ef18c0c5d8ce405039167e0963aa0ca7d0501ea9
Muhammad Rasel <rrasel141@gmail.com>
2023-09-02 23:58:39 +0600

    design: hero section

--- src/app/components/Search.js
+++ src/app/components/Search.js
@@ -5,6 +5,8 @@ import { useContext } from 'react'
 import { SearchContext } from '../context/search'
 
 // components
+import DateSelection from './DateSelection'
+import HoursSelection from './HoursSelection'
 import LocationSelection from './LocationSelection'
 
 export default function Search() {
@@ -17,8 +19,22 @@ export default function Search() {
           : 'bg-white rounded-[20px] py-6 xl:pr-4 xl:h-[98px]'
       } hidden xl:block w-full relative shadow-lg`}
     >
-      <div className='xl:h-full flex items-center px-6 xl:px-0'>
+      <div className={`h-full flex ${searchActive && 'container mx-auto'}`}>
         <LocationSelection />
+        <DateSelection />
+        <HoursSelection />
+        {/* btn */}
+        <div className='xl:h-full flex items-center px-6 xl:px-0'>
+          <button
+            className={`${
+              searchActive
+                ? 'btn btn-sm btn-accent xl:w-[164px]'
+                : 'btn btn-lg btn-accent xl:w-[184px]'
+            }`}
+          >
+            Search
+          </button>
+        </div>
       </div>
     </div>
   )
